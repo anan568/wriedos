709e37dba83b746224dad5cacb5d6ab3cf6457aa
anan568 <an570owo@gmail.com>
2025-12-12 23:51:12 -0600

    Update pwquality_stuff.sh

--- pwquality_stuff.sh
+++ pwquality_stuff.sh
@@ -3,3 +3,6 @@ sudo sed -i 's/^#\?dcredit.*/dcredit = -1/' /etc/security/pwquality.conf
 sudo sed -i 's/^#\?ucredit.*/ucredit = -1/' /etc/security/pwquality.conf
 sudo sed -i 's/^#\?lcredit.*/lcredit = -1/' /etc/security/pwquality.conf
 sudo sed -i 's/^#\?ocredit.*/ocredit = -1/' /etc/security/pwquality.conf
+
+set_pwq retry    3
+set_pwq difok    4
