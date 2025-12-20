f33583755cf7a00e7fc5a04d5ef8ce6f20e0dd8e
zxrkcr <alexgarcia131983@gmail.com>
2025-12-19 18:48:11 -0600

    SSH_config complete

--- ssh_config.sh
+++ ssh_config.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 #checks if being ran on root
 if [[ $EUID -ne 0 ]]; then
-    echo "Ensure to run this script as root"
+    echo "======Ensure to run this script as root"
     exit 1
 fi
 
@@ -13,13 +13,19 @@ fi
 SSHD_CONFIG="/etc/ssh/sshd_config"
 BACKUP="/etc/ssh/sshd_config.bak"
 
-echo "Updating SSH"
+echo "======Updating SSH"
 apt update -y
 apt install --only-upgrade -y openssh-server openssh-client
 
+#checking to make sure its enabled and active
+systemctl enable ssh
+systemctl start ssh
+
 # creating a backup
 cp -p "$SSHD_CONFIG" "$BACKUP"
-echo "[+] Backup created at $BACKUP"
+echo "====Backup created at $BACKUP"
+
+#function to safely set ssh configs
 set_config() {
     local key="$1"
     local value="$2"
@@ -31,28 +37,55 @@ set_config() {
     fi
 }
 
-echo "applying SSH setting configurations"
+echo "=======applying SSH setting configurations"
 
+# Authentication (PASSWORD-BASED, NO KEYS)
 set_config "PermitRootLogin" "no"
 set_config "PasswordAuthentication" "yes"
+set_config "PubkeyAuthentication" "no"
 set_config "PermitEmptyPasswords" "no"
-set_config "Protocol" "2"
 set_config "ChallengeResponseAuthentication" "no"
+set_config "UsePAM" "yes"
+# Connection hardening
 set_config "StrictModes" "yes"
 set_config "MaxAuthTries" "3"
-set_config "UsePAM" "yes"
-set_config "X11Forwarding" "no"
-set_config "PrintMotd" "no"
+set_config "LoginGraceTime" "30"
 set_config "ClientAliveInterval" "300"
 set_config "ClientAliveCountMax" "2"
+# Feature lockdown
+set_config "X11Forwarding" "no"
 set_config "AllowTcpForwarding" "no"
-set_config "LoginGraceTime" "30"
+set_config "PrintMotd" "no"
 
 
-echo "Checking validity of the configurations"
+echo "========Checking validity of the configurations"
 sshd -t
 
 #verifies and restarts to apply
 systemctl restart ssh || echo "Config error, check $SSHD_CONFIG"
 
-echo "ssh configurations settings applied"
+# setting up firewall config
+echo "=======Configuring UFW firewall"
+apt install ufw -y
+ufw allow ssh
+
+if ufw status | grep -q  "Status: active"; then
+    echo "=====Enabling UFW"
+    ufw --force enable
+else
+    echo "====UFW is enabled"
+fi
+
+#listing ssh group members
+if getent group ssh &>/dev/null; then
+    MEMBERS=$(getent group ssh | awk -F: '{print $3, $4}')
+    if [[ -n "$MEMBERS" ]]; then
+        echo "$MEMBERS"
+    else
+        echo "====ssh group exists but has no members"
+    fi
+else
+    echo "no 'ssh' group found in the group lists"
+fi
+
+echo "=========ssh configurations settings applied"
